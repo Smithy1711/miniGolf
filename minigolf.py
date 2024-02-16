a77957d9130ebe96213e97e52ed094d7e5b0396c
Smithy1711 <smith1711@hotmail.com>
2024-02-16 23:07:57 +0000

    main screen and button interaction

---
+++ minigolf.py
@@ -0,0 +1,82 @@
+import pygame as pg
+import sys
+
+class Game:
+    def __init__(self):
+        pg.init()
+
+        self.current_screen = "main"
+
+        self.screen_width = 800
+        self.screen_height = 600
+        self.screen = pg.display.set_mode((self.screen_width, self.screen_height))
+        pg.display.set_caption("Blank Screen")
+        self.clock = pg.time.Clock()
+        self.is_running = True
+
+        self.font = pg.font.Font(None, 36)  # None for default font, 36 is the font size
+
+        self.button_width = 250
+        self.button_height = 120
+        self.button_play_coord = ((self.screen_width - self.button_width)/2, (self.screen_height - self.button_height)/2)
+
+
+    def run(self):
+        while self.is_running:
+            if self.current_screen == "main":
+                self.loadMainScreen()
+                self.handle_main_screen_events()
+            elif self.current_screen == "game":
+                self.loadGameScreen()
+                
+
+            self.update()
+            self.clock.tick(60)
+
+    def loadMainScreen(self):
+        self.screen.fill((0, 255, 255))
+        pg.draw.rect(self.screen, (0, 102, 0), (0, self.screen_height*2/3, self.screen_width, self.screen_height // 3))
+        pg.draw.rect(self.screen, (128, 128, 128), (self.button_play_coord[0], self.button_play_coord[1], self.button_width, self.button_height))
+
+        play_text = self.font.render("Play", True, (0, 0, 0))
+
+        # Calculate the position for the text
+        text_x = self.screen_width // 2 - play_text.get_width() // 2  # Center horizontally
+        text_y = self.screen_height // 2 - play_text.get_height() // 2  # Center vertically within the lower third
+
+        # Blit the text surface onto the screen
+        self.screen.blit(play_text, (text_x, text_y))
+        pg.display.flip()
+
+    def loadGameScreen(self):
+        self.screen.fill((0, 255, 255))
+        pg.draw.rect(self.screen, (0, 102, 0), (0, self.screen_height*2/3, self.screen_width, self.screen_height // 3))
+        pg.display.flip()
+    
+    
+    def runGame(self):
+
+
+    def handle_main_screen_events(self):
+        for event in pg.event.get():
+            mouse_x, mouse_y = pg.mouse.get_pos()
+            if event.type == pg.QUIT:
+                self.is_running = False
+
+            if self.button_play_coord[0] < mouse_x < self.button_play_coord[0] + self.button_width and self.button_play_coord[1] < mouse_y < self.button_play_coord[1] + self.button_height:
+                pg.mouse.set_cursor(pg.SYSTEM_CURSOR_HAND)
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    self.current_screen = "game"
+            else:
+                pg.mouse.set_cursor(*pg.cursors.arrow)
+            
+
+    def update(self):
+        pass
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
+    pg.quit()
+    sys.exit()
